891558101f71b79e8719af4b413679b8d350b49d
amanda1686 <80174591+amanda1686@users.noreply.github.com>
2024-02-21 23:44:21 +0100

    finalizar-compra

--- PetHolidays-Frontend/src/main.jsx
+++ PetHolidays-Frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import FormularioReserva from './FinalizarReserva/FinalizarReserva.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <FormularioReserva/>
   </React.StrictMode>,
 )
