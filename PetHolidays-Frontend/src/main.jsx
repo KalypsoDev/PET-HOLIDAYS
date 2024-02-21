c8dabfcc27041eb5a5b455382fd75d2344a1399a
Maria <ary023@gmail.com>
2024-02-21 17:54:26 +0100

    page that list all apartments (user and admin versions)

--- PetHolidays-Frontend/src/main.jsx
+++ PetHolidays-Frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ApartmentShow from './components/card/ApartmentShow';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+
+        {/* modificar e indicar las rutas correctas, ahora pongo solo una pagina para pruebas */}
+
+        <Route path="/" element={<ApartmentShow isAdmin={true}/>} />
+        
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
+
