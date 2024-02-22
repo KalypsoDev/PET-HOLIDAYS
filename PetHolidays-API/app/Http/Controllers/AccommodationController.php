3ed55fb64f58068e19d39b583a032a35a432b74d
Angela <antunezangela89@gmail.com>
2024-02-22 18:33:41 +0100

    Import Model and create index function

--- PetHolidays-API/app/Http/Controllers/AccommodationController.php
+++ PetHolidays-API/app/Http/Controllers/AccommodationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Accommodation;
 
 class AccommodationController extends Controller
 {
@@ -12,7 +13,8 @@ class AccommodationController extends Controller
      */
     public function index()
     {
-        //
+        $accommodations = Accommodation::all();
+        return response()->json($accommodations, 200);
     }
 
     /**
