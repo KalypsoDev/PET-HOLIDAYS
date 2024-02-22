95ed701ea111f8a69974e865062354a6d67f1d24
Angela <antunezangela89@gmail.com>
2024-02-22 18:27:31 +0100

    Create all the routes for AccommodationController

--- PetHolidays-API/routes/api.php
+++ PetHolidays-API/routes/api.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccommodationController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AccommodationController::class)->group(function () {
+    Route::get('/accommodations', 'index');
+    Route::post('/accommodation', 'store');
+    Route::get('/accommodation/{id}', 'show');
+    Route::put('/accommodation/{id}', 'update');
+    Route::delete('/accommodation/{id}', 'destroy');
 });
