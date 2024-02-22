04b85f3895f598db77386bad13f6d4e74e969109
Marta <martalarena2002@gmail.com>
2024-02-22 09:27:58 +0100

    terminado bodyHome

--- PetHolidays-Frontend/src/Components/Slider/Slider.jsx
+++ PetHolidays-Frontend/src/Components/Slider/Slider.jsx
@@ -1,139 +1,32 @@
-import chair from "../../assets/chairs-2181947_1280.jpg"
-import kitchen from "../../assets/kitchen-2165756_1280.jpg"
-import living from "../../assets/living-room-1835923_1920.jpg"
-import man from "../../assets/man-2425121_1280.jpg"
-import dog from "../../assets/perro-casa.webp"
-
-
-
+import chair from "../../assets/chairs-2181947_1280.jpg";
+import kitchen from "../../assets/kitchen-2165756_1280.jpg";
+import living from "../../assets/living-room-1835923_1920.jpg";
+import man from "../../assets/man-2425121_1280.jpg";
+import dog from "../../assets/perro-casa.webp";
+import Carousel from "react-bootstrap/Carousel";
+import BodyHome from "../BodyHome/BodyHome";
 
 export default function Slider() {
   return (
-    <div id="default-carousel" class="relative w-full" data-carousel="slide">
-      <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src={chair}
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          ></img>
-        </div>
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src={living}
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          ></img>
-        </div>
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src={kitchen}
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          ></img>
-        </div>
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src={man}
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          ></img>
-        </div>
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src={dog}
-            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          ></img>
-        </div>
-      </div>
-
-      <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="true"
-          aria-label="Slide 1"
-          data-carousel-slide-to="0"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 2"
-          data-carousel-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 3"
-          data-carousel-slide-to="2"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 4"
-          data-carousel-slide-to="3"
-        ></button>
-        <button
-          type="button"
-          class="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 5"
-          data-carousel-slide-to="4"
-        ></button>
-      </div>
-
-      <button
-        type="button"
-        class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-prev
-      >
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span class="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        type="button"
-        class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-next
-      >
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span class="sr-only">Next</span>
-        </span>
-      </button>
-    </div>
+    <>
+      <Carousel data-bs-theme="dark">
+        <Carousel.Item>
+          <img className="d-block w-100" src={man} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={kitchen} alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={living} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={chair} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={dog} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
+      <BodyHome />
+    </>
   );
 }
