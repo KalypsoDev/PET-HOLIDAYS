32ba89119aa4d99269ff156456f76fa984b3b669
Angela <antunezangela89@gmail.com>
2024-02-22 18:54:18 +0100

    Import AccommodationRequest and create store function (create is unnecessary)

--- PetHolidays-API/app/Http/Controllers/AccommodationController.php
+++ PetHolidays-API/app/Http/Controllers/AccommodationController.php
@@ -5,46 +5,29 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Accommodation;
+use App\Http\Requests\AccommodationRequest;
 
 class AccommodationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $accommodations = Accommodation::all();
         return response()->json($accommodations, 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function store(AccommodationRequest $request)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        Accommodation::create($request->all());
+        return response()->json([
+            'success' => true
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
     public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
     {
         //
     }
